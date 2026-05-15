2964a1d420f8969bb9d0ffe665cbe00e85e29d9e
Lucas Ainsworth <lucas.ainsworth@cloudinary.com>
2026-05-15 07:29:13 -0700

    Update site-config.js

--- js/site-config.js
+++ js/site-config.js
@@ -7,8 +7,8 @@ window.SITE_CONFIG = {
   CLOUD_NAME: "dz6ajwh6k",
   LIST_TAG: "selfies-demo",
   UPLOAD_PRESET: "selfies-demo",
-  /** Full HTTPS URL to mobile.html, e.g. https://YOUR_USER.github.io/cloudinary-selfie-demo/mobile.html */
-  MOBILE_PAGE_URL: "",
+  /** Full HTTPS URL to mobile.html (used for QR on the display). */
+  MOBILE_PAGE_URL: "https://lucasainsworth-cld.github.io/cloudinary-selfie-demo/mobile.html",
   POLL_MS: 15000,
   MAX_VISIBLE: 40,
   ALLOW_GRID_SCROLL: false,
